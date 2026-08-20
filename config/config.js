ce6b0e555406a3bc3305929d0592114bc5a8e340
Yuliia Voziian <vozianulia@gmail.com>
2026-08-20 14:59:04 +0200

    +

--- config/config.js
+++ config/config.js
@@ -3,6 +3,8 @@ window.siteConfigData = {
   "logo": "assets/images/logo.svg",
   "favicon": "assets/images/favicon.svg",
   "email": "hello@pestora.example",
+  "phone": "(+01) 234-5678",
+  "location": "35th Street, LA",
   "disclaimer": "Pestora is an independent pest control connection platform. We do not directly perform pest control services. Availability, pricing and provider terms are determined by independent local providers.",
   "formSuccessMessage": "Thank you! We have successfully received your request. We will review your information and reply by email shortly."
 };
